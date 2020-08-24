36018d3d9543f6889c0d1177f3757543a3a42d9e
Tamer Hamoud <67854476+TamerHa-coder@users.noreply.github.com>
2020-08-24 15:42:10 +0300

    test 1+2 backend, test 1 client
    Adding get request that returns all the tickets if no query param, and filtered tickets with query param.
    
    changing the title name + changing the favicon.

--- server/app.js
+++ server/app.js
@@ -1,5 +1,28 @@
 const express = require('express');
 const app = express();
+const fs = require('fs').promises;
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.get('',  (req, res) => {    
+    res.send('Hello');
+})
+
+app.get('/api/tickets', async (req, res) => {
+    const content = await fs.readFile('./data.json');
+    const tickets = JSON.parse(content);
+    const query = req.query.searchText;
+    if (!query){
+        res.send(tickets);
+    }
+     else{
+        queryTickets = tickets.filter((ticket) => ((ticket.title).toLowerCase()).includes(query));
+        res.send(queryTickets);
+    }
+   
+})
+
 
 
 module.exports = app;
