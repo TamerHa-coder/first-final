eaf24603785d1230a24457c544ecb420f0f49952
Tamer Hamoud <67854476+TamerHa-coder@users.noreply.github.com>
2020-08-24 17:26:46 +0300

    Adding post request

--- server/app.js
+++ server/app.js
@@ -20,7 +20,36 @@ app.get('/api/tickets', async (req, res) => {
         queryTickets = tickets.filter((ticket) => ((ticket.title).toLowerCase()).includes(query));
         res.send(queryTickets);
     }
-   
+})
+
+app.post('/api/tickets/:ticketId/done', async (req, res) => {
+    let content = await fs.readFile('./data.json');
+    const tickets = JSON.parse(content);
+    const index = tickets.findIndex(ticket => ticket.id === req.params.ticketId);
+    if(index >= 0){
+        tickets[index].done = true;
+        content = JSON.stringify(tickets, null, 2);
+        await fs.writeFile('./data.json', content);
+        res.send(tickets[index]);
+    } else {
+        res.send('Error');
+    }
+
+})
+
+app.post('/api/tickets/:ticketId/undone', async (req, res) => {
+    let content = await fs.readFile('./data.json');
+    const tickets = JSON.parse(content);
+    const index = tickets.findIndex(ticket => ticket.id === req.params.ticketId);
+    if(index >= 0){
+        tickets[index].done = false;
+        content = JSON.stringify(tickets, null, 2);
+        await fs.writeFile('./data.json', content);
+        res.send(tickets[index]);
+    } else {
+        res.send('Error');
+    }
+
 })
 
 
