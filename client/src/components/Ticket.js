493d74a21bac56cc38e776045e06696f5a7c9abd
Tamer Hamoud <67854476+TamerHa-coder@users.noreply.github.com>
2020-08-26 11:27:05 +0300

    completing all client tests

--- client/src/components/Ticket.js
+++ client/src/components/Ticket.js
@@ -1,3 +1,21 @@
 import React from 'react';
-import axios from 'axios';
 
+
+
+const Tickets = ({id, title,content ,userEmail, creationTime, labels, onHide}) =>{
+return(
+    <li key={id} className="ticket">
+        <span className="ticketOptions">
+            <button className="hideTicketButton" onClick={() => onHide(id)}><i className="far fa-eye-slash"></i></button>
+        </span>
+        <h3 className="title">{title}</h3>
+        <p>{content}</p>
+        <footer>
+        <div className="email">by {userEmail} | { new Date(creationTime).toLocaleString() }</div>
+        <div>{labels ? labels.map(label => (<span className="label">{label}</span>)) : undefined}</div>
+        </footer>
+    </li>
+)
+}
+
+export default Tickets;
